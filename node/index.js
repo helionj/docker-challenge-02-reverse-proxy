c9d23b06e199b165752fed6a338f39a9487bea39
helionj <helionporto@hotmail.com>
2022-11-04 08:21:36 -0400

    first commit

---
+++ node/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const mysql = require('mysql')
+const app = express()
+const port = 3000
+
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+}
+
+
+
+app.use(express.json())
+
+app.get('/', (req,res) => {
+ 
+  insert()
+  const sqlQuery = 'SELECT * from people'
+  execSQLQuery(sqlQuery, res)
+})
+
+app.listen(port, () => {
+  console.log('Server running')
+})
+
+const execSQLQuery = (sqlQry, res) => {
+  const connection = mysql.createConnection(config);
+
+  connection.query(sqlQry, (error, results, fields) => {
+      if(error) 
+        res.json(error);
+      else
+        res.send(`<h1>Full Cycle Rocks</h1>${results.map(result => JSON.stringify(result))}`);
+      connection.end();
+      console.log('executou!');
+  })
+}
+const insert = () => {
+  const connection = mysql.createConnection(config)
+  const sql = `INSERT into people(name) values('Tobias')`
+  connection.query(sql)
+  connection.end()
+}
+
